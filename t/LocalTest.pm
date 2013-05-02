25370c14b18e331d109f4eb20f3f4507e0c9c030
Guillaume Aubert <aubertg@cpan.org>
2013-05-01 21:21:54 -0400

    Updated copyright year.

--- t/LocalTest.pm
+++ t/LocalTest.pm
@@ -341,7 +341,7 @@ L<http://search.cpan.org/dist/Data-Validate-Type/>
 
 =head1 COPYRIGHT & LICENSE
 
-Copyright 2012 Guillaume Aubert.
+Copyright 2012-2013 Guillaume Aubert.
 
 This program is free software: you can redistribute it and/or modify it under
 the terms of the GNU General Public License version 3 as published by the Free
