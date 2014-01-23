6a27bda075419610c46a00232d7093ce9ea61c16
Guillaume Aubert <aubertg@cpan.org>
2014-01-22 18:15:13 -0800

    Update copyright year.

--- lib/Data/Validate/Type.pm
+++ lib/Data/Validate/Type.pm
@@ -986,7 +986,7 @@ encapsulation for Params::Util and I learnt quite a bit from it.
 
 =head1 COPYRIGHT & LICENSE
 
-Copyright 2012-2013 Guillaume Aubert.
+Copyright 2012-2014 Guillaume Aubert.
 
 This program is free software: you can redistribute it and/or modify it under
 the terms of the GNU General Public License version 3 as published by the Free
